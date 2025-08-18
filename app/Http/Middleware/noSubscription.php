d070fb1a013ebecd7ca923084e0ffa402e9d1bd1
Carlos Magno <carmaso_@hotmail.com>
2025-08-18 10:50:02 -0300

    feat(middleware): add subscription status middlewares

---
+++ app/Http/Middleware/noSubscription.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class noSubscription
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // check ir the user has no subscription
+        if(auth()->user()->subscribed(env("STRIPE_PRODUCT_ID"))){
+            return redirect()->route("dasboard");
+        }
+        return $next($request);
+    }
+}
